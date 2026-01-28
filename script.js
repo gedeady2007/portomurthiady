50faae1e3ba562747aa46943e8807482ffd1d0a7
gedeady2007 <gedeadyjayamurthi2007@gmail.com>
2026-01-28 23:39:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.getElementById("menu-icon");
+    const navbar = document.querySelector(".navbar");
+
+    menuIcon.addEventListener("click", () => {
+        navbar.classList.toggle("active");
+        console.log("MENU TOGGLED"); // DEBUG
+    });
+});
